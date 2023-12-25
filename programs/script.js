a8b2e61d590cbb48a6f5bbb12c1c3d44c987bfc4
imadityaxd <104629524+imadityaxd@users.noreply.github.com>
2023-12-25 21:30:35 +0530

    25Decemeber2023

---
+++ programs/script.js
@@ -0,0 +1,12 @@
+function showInput() {
+    // Get the input element by its id
+    let userInput = document.getElementById("userInput").value;
+
+    // Displaying the input
+    console.log("You entered: " + userInput);
+
+    var num = parseInt(userInput);
+
+    console.log(5 + num);
+}
+
